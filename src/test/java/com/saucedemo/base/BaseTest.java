7a3dfefd40fbe74024d29541fb2726461ce385d7
vdmitriv15 <64485743+vdmitriv15@users.noreply.github.com>
2025-10-24 15:48:50 -0400

    Add TestNG suite configuration

---
+++ src/test/java/com/saucedemo/base/BaseTest.java
@@ -0,0 +1,31 @@
+package com.saucedemo.base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+        driver = new ChromeDriver(options);
+        driver.manage().deleteAllCookies();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
---
+++ src/test/java/com/saucedemo/tests/LoginTest.java
@@ -0,0 +1,17 @@
+package com.saucedemo.tests;
+
+import com.saucedemo.base.BaseTest;
+import com.saucedemo.pages.InventoryPage;
+import com.saucedemo.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LoginTest extends BaseTest {
+
+    @Test(description = "Verify that a standard user can log in successfully")
+    public void testSuccessfulLogin() {
+        LoginPage loginPage = new LoginPage(driver).open();
+        InventoryPage inventoryPage = loginPage.loginAs("standard_user", "secret_sauce");
+        Assert.assertTrue(inventoryPage.isLoaded(), "Inventory page should be displayed after login");
+    }
+}
